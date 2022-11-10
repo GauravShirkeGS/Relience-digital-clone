d27e9c0b072ce74c58629bd60d9b004f9c1b7262
neerajsinghmahar11 <neeraj.singh.mahar11@gmail.com>
2022-11-10 18:14:33 +0530

    fixed navbar and logo text

--- components.js
+++ components.js
@@ -1,6 +1,9 @@
 const Navbar=()=>{
-    return `<div id="Logo">
+    return `
+    <div id="allnavbar">
+    <div id="Logo">
     <img src="https://i.ibb.co/WvkVDSz/2.png" alt="">
+    <span id="logo_text">GO SHOP</span>
 </div>
 <div id="Search-Bar">
     <input type="text" placeholder="Find your favorite products" id="Search-Input">
@@ -9,6 +12,7 @@ const Navbar=()=>{
 <div id="CartLogin">
     <div id="Cart"><i class="fa-solid fa-cart-shopping"></i><cart-count id="Cart-Count">0</cart-count><span> Cart</span></div>
     <div id="User"><i class="fa-solid fa-user"></i><span> Login</span></div>
+</div>
 </div>`
 }
 const CategoryPanel=()=>{
