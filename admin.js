b634d297463edac2ff6de4f9a98fbbae36d23422
gauravshirkegs <shirkegaurav611@gmail.com>
2022-11-14 15:29:07 +0530

    admin page

--- admin.js
+++ admin.js
@@ -130,3 +130,176 @@ let res = await fetch (`https://636e114fb567eed48ad1f04d.mockapi.io/${category}/
 })
 
  }
+
+ document.getElementById('apple_iphone').onclick = () => {
+    let g =document.getElementById('apple_iphone').value;
+    get_data(g)
+ }
+
+ 
+ document.getElementById('tv').onclick = () => {
+    let tv =document.getElementById('tv').value;
+    get_data(tv)
+ }
+
+ 
+ document.getElementById('musicProduct').onclick = () => {
+    let musicProduct =document.getElementById('musicProduct').value;
+    get_data(musicProduct)
+ }
+
+ 
+ document.getElementById('watches').onclick = () => {
+    let watches =document.getElementById('watches').value;
+    get_data(watches)
+ }
+
+ 
+ document.getElementById('other').onclick = () => {
+    let other =document.getElementById('other').value;
+    get_data(other)
+ }
+
+
+
+ const get_data = async (key) => {
+   
+    
+  
+    // window.location.reload();
+    if(key=="tv"||key=="musicProduct"){
+        let res = await fetch (`https://636d5048b567eed48abef381.mockapi.io/${key}`)
+    
+        let data = await res.json();
+         console.log('data:', data)
+        append_data(data);
+    }else{
+        let res = await fetch (`https://636e114fb567eed48ad1f04d.mockapi.io/${key}`)
+    
+        let data = await res.json();
+    append_cards (data);
+    }
+
+ }
+
+
+
+
+ 
+ const append_cards = async (data) => {
+  
+    let added = document.getElementById('added');
+    added.innerHTML= null;
+
+    data.forEach((el) => {
+
+        let div= document.createElement('div');
+        div.setAttribute('class','item');
+        div.onclick = () => {
+             cardfun(el);
+        }
+
+        let img= document.createElement('img');
+        img.src = el.image;
+
+        let name= document.createElement('p');
+        name.innerText = el.name;
+        name.setAttribute('class','name');
+
+
+        let price= document.createElement('p');
+        price.innerText= `Rs: ${el.price}`
+        
+
+        let delete_btn= document.createElement('button');
+        delete_btn.setAttribute('class','offers');
+        delete_btn.innerText="Delete"
+      
+         let change = document.createElement('div');
+
+         let input = document.createElement('input');
+         input.placeholder = "Update price";
+
+        let change_btn = document.createElement('button');
+        change_btn.innerText= 'Update'
+
+        change.append(input,change_btn);
+ 
+        let btn_div = document.createElement('div');
+        btn_div.append(change,delete_btn);
+
+        let name_div = document.createElement('div');
+        name_div.append(name,price)
+        name_div.setAttribute('class','name_dev')
+
+        div.append(img,name_div,btn_div)
+
+       added.append(div);
+
+
+        
+    });
+
+
+ }
+
+
+ 
+ const append_data = async (data) => {
+  
+    let added = document.getElementById('added');
+    added.innerHTML= null;
+
+    data.forEach((el) => {
+
+        let div= document.createElement('div');
+        div.setAttribute('class','item');
+        div.onclick = () => {
+             cardfun(el);
+        }
+
+        let img= document.createElement('img');
+        img.src = el.imageURL;
+
+        let name= document.createElement('p');
+        name.innerText = el.title;
+        name.setAttribute('class','name');
+
+
+        let price= document.createElement('p');
+        price.innerText= `Rs: ${el.price}`
+        
+
+        let delete_btn= document.createElement('button');
+        delete_btn.setAttribute('class','offers');
+        delete_btn.innerText="Delete"
+      
+         let change = document.createElement('div');
+
+         let input = document.createElement('input');
+         input.placeholder = "Update price";
+
+        let change_btn = document.createElement('button');
+        change_btn.innerText= 'Update'
+
+        change.append(input,change_btn);
+ 
+        let btn_div = document.createElement('div');
+        btn_div.append(change,delete_btn);
+
+        let name_div = document.createElement('div');
+        name_div.append(name,price)
+        name_div.setAttribute('class','name_dev')
+
+        div.append(img,name_div,btn_div)
+
+       added.append(div);
+
+
+        
+    });
+
+
+ }
+
+ get_data ('apple_iphone')
